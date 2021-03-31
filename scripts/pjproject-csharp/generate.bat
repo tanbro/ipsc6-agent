dc620989f6d45bf87b19e6316ff8a4971c975d25
Liu Xue Yan <liu_xue_yan@foxmail.com>
2021-03-31 14:10:31 +0800

    add: windows swig generate script file

---
+++ scripts/pjproject-csharp/generate.bat
@@ -0,0 +1,17 @@
+@echo off
+
+set PJPROJECT_ROOT=submodules\pjproject
+
+swig -v ^
+    -I%PJPROJECT_ROOT%\pjlib\include ^
+	-I%PJPROJECT_ROOT%\pjlib-util\include ^
+	-I%PJPROJECT_ROOT%\pjmedia\include ^
+	-I%PJPROJECT_ROOT%\pjsip\include ^
+	-I%PJPROJECT_ROOT%\pjnath\include ^
+    -I%PJPROJECT_ROOT%\pjsip\include ^
+    -c++ -w312 ^
+    -csharp -namespace org.pjsip.pjsua2 ^
+    -outdir org.pjsip.pjsua2\src ^
+    %PJPROJECT_ROOT%\pjsip-apps\src\swig\pjsua2.i
+
+move %PJPROJECT_ROOT%\pjsip-apps\src\swig\pjsua2_wrap.* pjsua2_wrap\src
