be325cad8d10258ff961a4d8c28085abd579c18f
Liu Xue Yan <liu_xue_yan@foxmail.com>
2021-04-30 15:18:38 +0800

    fixup! A generic StateChange event arguments class

---
+++ ipsc6-agent-client/TeleState.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ipsc6.agent.client
+{
+    public enum TeleState
+    {
+        HangUp = 0,
+        OffHook = 1,
+    }
+}
