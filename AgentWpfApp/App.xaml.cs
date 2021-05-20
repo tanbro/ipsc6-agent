905269fbcd13a823ac2b00153fb689bd66d748fe
Liu Xue Yan <liu_xue_yan@foxmail.com>
2021-05-20 17:21:01 +0800

    许多样式重排

--- AgentWpfApp/App.xaml.cs
+++ AgentWpfApp/App.xaml.cs
@@ -23,27 +23,28 @@ namespace AgentWpfApp
 
             try
             {
-                logger.Debug("Connector.Initial");
-                Connector.Initial();
-                try
-                {
-                    if (new LoginWindow().ShowDialog() == true)
-                    {
-                        logger.Debug("登录成功");
-                        MessageBox.Show("登录成功");
-                        new MainWindow().ShowDialog();
-                    }
-                    else
-                    {
-                        logger.Debug("登录失败");
-                        MessageBox.Show("登录失败");
-                    }
-                }
-                finally
-                {
-                    logger.Debug("Connector.Release");
-                    Connector.Release();
-                }
+                new MainWindow().ShowDialog();
+                //logger.Debug("Connector.Initial");
+                //Connector.Initial();
+                //try
+                //{
+                //    if (new LoginWindow().ShowDialog() == true)
+                //    {
+                //        logger.Debug("登录成功");
+                //        MessageBox.Show("登录成功");
+                //        new MainWindow().ShowDialog();
+                //    }
+                //    else
+                //    {
+                //        logger.Debug("登录失败");
+                //        MessageBox.Show("登录失败");
+                //    }
+                //}
+                //finally
+                //{
+                //    logger.Debug("Connector.Release");
+                //    Connector.Release();
+                //}
             }
             finally
             {
