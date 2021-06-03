be4c1c7272d4f34a9e53ea06592b1590acaa06e5
Liu Xue Yan <liu_xue_yan@foxmail.com>
2021-06-03 10:49:54 +0800

    modify loggings

--- ipsc6.agent.wpfapp/Config/Manager.cs
+++ ipsc6.agent.wpfapp/Config/Manager.cs
@@ -12,7 +12,7 @@ using System.Diagnostics;
 
 namespace ipsc6.agent.wpfapp.Config
 {
-    class Manager
+    static class Manager
     {
         static readonly log4net.ILog logger = log4net.LogManager.GetLogger(typeof(Manager));
 
@@ -28,36 +28,24 @@ namespace ipsc6.agent.wpfapp.Config
         {
             Assembly assembly = Assembly.GetExecutingAssembly();
             FileVersionInfo versionInfo = FileVersionInfo.GetVersionInfo(assembly.Location);
+            var cmdArgs = Environment.GetCommandLineArgs();
+
+            if (cmdArgs.Length > 1)
+                logger.DebugFormat("CommandLineArgs: {0}", string.Join(" ", cmdArgs));
 
             var builder = new ConfigurationBuilder()
                 .SetBasePath(Directory.GetCurrentDirectory())
                 .AddJsonFile(Path.Combine(new string[] {
                     "Config", "settings.json"
-                }),
-                optional: false)
+                }), optional: false)
                 .AddJsonFile(Path.Combine(new string[] {
                     Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
                     versionInfo.ProductName, "User", "settings.json"
                 }), optional: true)
-                .AddEnvironmentVariables(prefix: "IPSC6AGENT_");
-
-            var args = Environment.GetCommandLineArgs();
-            if (args.Length > 0)
-            {
-                logger.DebugFormat("args = {0}", string.Join(", ", args));
-                builder.AddCommandLine(args);
-            }
+                .AddEnvironmentVariables(prefix: "IPSC6AGENT_")
+                .AddCommandLine(cmdArgs);
 
             configurationRoot = builder.Build();
-
-            {
-                var cfg = ConfigurationRoot;
-                var options = new Ipsc();
-                cfg.GetSection(nameof(Ipsc)).Bind(options);
-                logger.DebugFormat("ServerList: {0}", string.Join(", ", options.ServerList));
-                logger.DebugFormat("Name: {0}", options.Name);
-            }
-
             return ConfigurationRoot;
         }
 
