a8f6cc046d814b2cde95ac6b77573f02a3f9685d
Liu Xue Yan <liu_xue_yan@foxmail.com>
2021-06-21 12:42:06 +0800

    remove some unused

--- ipsc6.agent.wpfapp/ViewModels/LoginViewModel.cs
+++ ipsc6.agent.wpfapp/ViewModels/LoginViewModel.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.ComponentModel;
+
 using Microsoft.Extensions.Configuration;
 
 using Microsoft.Toolkit.Mvvm.Input;
